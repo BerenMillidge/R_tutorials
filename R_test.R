9b2844219ec68279f011586731b1399c26d9cd06
Beren Millidge <bmillidgework@gmail.com>
2018-07-17 22:46:19 +0100

    Add files via upload

--- R_test.R
+++ R_test.R
@@ -50,3 +50,45 @@ A[,1]
 # can also extract vetors of rows for instance i.e. first and third columns
 A[, c(1,3)]
 # that is cool, and very flexible... nice!
+
+# a list is a heterogeneous data structure which can contain objects of any ttpe
+# slice with square brackets, which just creates a new list with one element
+x <- list(1,2,"three")
+x[2]
+# to get the actual member use double square bracket
+x[[2]]
+
+# lists can also have named references like vectors
+
+# a dataframe is often the key structure for storing tables of data. It is a list of vectors
+# each element in the list is a different column of the table, this means that the columns can have 
+# arbitrary type but the type of each column element needs to be the same
+# which is precisely what makes sense for most data tables
+# created by data.frame function i.e.
+n <- c(2,3,4)
+s <- c("a", "b", "c")
+b <- c(TRUE, FALSE, TRUE)
+df <- data.frame(n,s,b)
+df
+
+
+# built in dataframes
+mtcars[1,2]
+head(mtcars)
+
+nrow(mtcars)
+
+# so yeah r allows lots of easy analysis on data frames and very useful methods to create these things
+# does not seem difficult at all which is nice!
+mtcars[["mpg"]] #use double square bracket notation to get the actualvalue of the column
+# can access dataframe column by name or by index
+mtcars[[2]] # double index notation both times
+# single index notation gets you a column slice
+mtcars[1]
+# this gets you the row labels as well, instead of just the values
+#can get multiple slices by giving it a vector
+mtcars[c(1,2)]
+# you can get row indexes too using a comma index
+mtcars[1,]
+
+demo(graphics)
